367fa4ba2e56e1ea700c529ce9574ecbe3004714
Kim Nguyen <design@kimisgold.com>
2013-06-10 12:45:18 -0400

    Add views for the browsing themes and individual themes.

---
+++ themes.php
@@ -0,0 +1,33 @@
+<?php include('header.php'); ?>
+<body class="themes">
+
+    <header>
+        <h1 id="site-title"><div class="logo"></div><a href="index">Hidden Histories<br>of the<br> National Mall</a></h1>
+        
+        <?php include('search.php'); ?>
+        
+        <?php include('navigation.php'); ?>
+    </header>
+
+    <div role="main">
+    
+        <h1>Themes</h1>
+        
+        <div class="theme-list">
+            <div class="theme"><a href="theme">military history</a></div>
+            <div class="theme"><a href="theme">everyday life</a></div>
+            <div class="theme"><a href="theme">building the mall</a></div>
+            <div class="theme"><a href="theme">work & play</a></div>
+            <div class="theme"><a href="theme">the neighborhood</a></div>
+            <div class="theme"><a href="theme">ghost mall</a></div>
+            <div class="theme"><a href="theme">environment</a></div>
+            <div class="theme"><a href="theme">commerce & trade</a></div>
+            <div class="theme"><a href="theme">design & monuments</a></div>
+            <div class="theme"><a href="theme">politics & protest</a></div>
+            <div class="theme"><a href="theme">D.C. history</a></div>
+            <div class="theme"><a href="theme">Design and Monuments</a></div>
+        </div>
+        
+    </div>
+    
+<?php include('footer.php'); ?>
