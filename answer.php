c299ff2f584d0c469e71a427245827b382904c72
Kim Nguyen <design@kimisgold.com>
2013-06-10 13:05:07 -0400

    Include other options for exploring related questions and content.

--- answer.php
+++ answer.php
@@ -13,29 +13,121 @@
     
         <h1>Why is there a lockkeeper's house on the Mall?</h1>
         
-        <div class="answer">
+        <article class="answer">
         
             <p>Until 1872, a canal covered the land that is now Constitution Avenue. Between 1835 and 1855, a lockkeeper and his family lived in the small stone house at the lock where the C & O Canal Extension joined the Washington City Canal and the Chesapeake and Ohio (C & O) Canal.</p>
     
-            <p>In the early nineteenth century, canals were the chief means of moving freight and bulk cargoes between ports and markets. In Washington, these waterways surrounded and criss-crossed the National Mall connecting  the Anacostia (then called the Eastern Branch) and the Potomac Rivers to each other and to a series of inland waterways.</p>        
-        
-        </div>
-        
-        <div class="learn-more">
+            <p>In the early nineteenth century, canals were the chief means of moving freight and bulk cargoes between ports and markets. In Washington, these waterways surrounded and criss-crossed the National Mall connecting  the Anacostia (then called the Eastern Branch) and the Potomac Rivers to each other and to a series of inland waterways.</p>
+            
+            <div class="related-items-mobile">
+            
+                <span class="previous"><a href="#">&lt;</a></span>
+            
+                <a href="#"><div class="image"></div></a>
+                                
+                <span class="next"><a href="#">&gt;</a></span>
 
-            <h3>Learn more about&hellip;</h3>
+                <div class="caption"><a href="#">Washington City Canal Lottery Ticket</a></div>
+
+                <p>In 1791, decades before the lockkeeper's house was built, Pierre Charles L'Enfant first proposed constructing the Washington City Canal. L'Enfant intended the canal to supply markets built along its path, ensuring the flow of commerce in the city. Lotteries conducted to finance construction  failed to raise enough money. Finally, in 1809, Congress authorized $100,000 for the canal which formally opened in 1815, delayed by the city's destruction in the War of 1812.</p>
+
+            </div>
+            
+            <div class="related-items">
+            
+                <div class="item">
                 
-            <ul>
-                <li><a href="#">The Lockkeeper's House</a></li>
-            </ul>
+                    <div class="image"></div>
+                    
+                     <h2><a href="#">View of Washington / drawn from nature and on stone by E. Sachse</a></h2>
 
-            <p class="back"><a href="explorations">Explore other questions about the National Mall</a></p>
+                      <p>Barges travelling on the Washington City Canal brought firewood and coal, marble and stone for public buildings, and food supplies, but the Canal was too shallow for large boats. Its banks frequently overflowed into surrounding streets during high tide, creating a marshy terrain on the Mall. The Canal flowed from the Anacostia River towards the Capitol, crossing the National Mall to join Tiber Creek, an inland extension of the Potomac River. It then entered the Potomac just south of the White House.</p>
+                
+                </div>
+
+                <div class="item">
+                
+                    <div class="image"></div>
+                    
+                     <h2><a href="#">Canal ("Washington Canal"), Washington, D.C. Lock details and sections</a></h2>
+
+                      <p>By 1833, the C&O Extension connected the Washington City Canal to the Chesapeake and Ohio Canal.  In 1835, the  lockkeeper living in the newly-constructed house on the corner of present-day 17th Street and Constitution Avenues managed traffic between canals and maintained canal records. The lockkeeper raised and lowered the canal locks, likely a manually-operated sluice gate allowing him to control the flow of water for passage of boats and their cargo. Some accounts report that the lockkeeper lived in the small stone building with 13 children.</p>
+                
+                </div>
+                
+                <div class="item">
+                
+                    <div class="image"></div>
+                    
+                     <h2><a href="#">West front of Capitol, vii, peristyle complete</a></h2>
+
+                      <p>Over the decades, financial problems, conflicts between builders and contractors and between local and federal governments, floods, pestilence, right-of-way disputes, and finally, the advent of railroads, stalled canal growth and clean-up. The lockkeeper's house was abandoned in 1855.  It served for a time as a squatters' tenement and later for the park police as a holding cell for prisoners. In the 1870s, dismayed by the downtrodden condition of the nation's capital and the unsanitary conditions of the Canal, the city began to fill in the waterway, making way for today's Constitution Avenue. The Lockkeeper's House remained as a visible remembrance of the city's early commercial dreams.</p>
+                      
+                </div>
+                      
+                <div class="item">
+                
+                    <div class="image"></div>
+                    
+                     <h2><a href="#">Old Locks house still standing</a></h2>
+
+                      <p>Over the decades, financial problems, conflicts between builders and contractors and between local and federal governments, floods, pestilence, right-of-way disputes, and finally, the advent of railroads, stalled canal growth and clean-up. The lockkeeper's house was abandoned in 1855.  It served for a time as a squatters' tenement and later for the park police as a holding cell for prisoners. In the 1870s, dismayed by the downtrodden condition of the nation's capital and the unsanitary conditions of the Canal, the city began to fill in the waterway, making way for today's Constitution Avenue. The Lockkeeper's House remained as a visible remembrance of the city's early commercial dreams.</p>
+                
+                </div>
             
-            <p class="prev"><a href="#">Previous question: Besides squirrels and birds, what animals lived on the Mall?</a></p>
+            </div>
+
+        </article>
+
+        <aside class="learn-more">
             
-            <p class="next"><a href="#">Next question: Which natural disasters have impacted the Mall and how?</a></p>
+            <div class="other-questions">
+
+                <p class="back"><a href="explorations">Go back to all questions about the National Mall</a></p>
+                
+                <p class="prev"><a href="#">Previous question: Besides squirrels and birds, what animals lived on the Mall?</a></p>
+                
+                <p class="next"><a href="#">Next question: Which natural disasters have impacted the Mall and how?</a></p>
+            
+            </div>
+
+            <div class="items">
+            
+                <h3>Learn more about&hellip;</h3>
+                    
+                <ul>
+                    <li><a href="#">The Lockkeeper's House</a></li>
+                    <li><a href="#">Washington City Canal Lottery Ticket</a></li>
+                    <li><a href="#">View of Washington / drawn from nature and on stone by E. Sachse</a></li>
+                    <li><a href="#">Canal ("Washington Canal"), Washington, D.C. Lock details and sections</a></li>
+                    <li><a href="#">West front of Capitol, vii, peristyle complete</a></li>
+                    <li><a href="#">Old Locks house still standing</a></li>
+                </ul>
+            
+            </div>
+
+
+            <div class="themes">
+            
+                <h3>Explore other questions about&hellip;</h3>
+                
+                <ul>
+                
+                    <li><a href="explorations"><span class="theme">building the mall</span></a></li>
+                    
+                    <li><a href="explorations"><span class="theme">the neighborhood</span></a></li>
+                    
+                    <li><a href="explorations"><span class="theme">ghost mall</span></a></li>
+                    
+                    <li><a href="explorations"><span class="theme">environment</span></a></li>
+                    
+                    <li><a href="explorations"><span class="theme">commerce &amp; trade</span></a></li>
+                </ul>
+                
+            </div>
         
-        </div>
-        
+        </aside>
+                
     </div>
+    
 <?php include('footer.php'); ?>
