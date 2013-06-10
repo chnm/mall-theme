31d182ed6ab3860ccb8ecbfb21768f49a7ed97e9
Kim Nguyen <design@kimisgold.com>
2013-06-10 12:58:18 -0400

    Reorganize navigation.

--- navigation.php
+++ navigation.php
@@ -1,6 +1,12 @@
 <nav id="navigation">
     <h1>Start exploring the National Mall by&hellip;</h1>
     <ul>
+        <li class="map">
+            <h2><a href="#">Map</a></h2>
+        </li>
+        <li class="explorations">
+            <h2><a href="explorations">Explorations</a></h2>
+        </li>
         <li class="people">
             <h2><a href="people-image">People</a></h2>
         </li>
@@ -11,13 +17,7 @@
             <h2><a href="sites">Sites</a></h2>
         </li>
         <li class="explorations">
-            <h2><a href="explorations">Explorations</a></h2>
-        </li>
-        <li class="explorations">
-            <h2><a href="explorations">Themes</a></h2>
-        </li>
-        <li class="map">
-            <h2><a href="#">Map</a></h2>
+            <h2><a href="themes">Themes</a></h2>
         </li>
     </ul>
 </nav>
