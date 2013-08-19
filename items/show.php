1379df6536625e6361cb8686846a8d235378ba77
Kim Nguyen <design@kimisgold.com>
2013-08-19 16:43:03 -0400

    Move item navigation in markup.

--- items/show.php
+++ items/show.php
@@ -4,6 +4,13 @@
 
 <h1><?php echo metadata('item', array('Dublin Core', 'Title')); ?></h1>
 
+    <nav>
+        <ul class="item-pagination navigation">
+            <li id="previous-item" class="previous"><?php echo link_to_previous_item_show(); ?></li>
+            <li id="next-item" class="next"><?php echo link_to_next_item_show(); ?></li>
+        </ul>
+    </nav>
+
 <aside>
     <?php if (metadata('item', 'has files')): ?>
     <?php $itemFiles = $item->Files; ?>
@@ -24,13 +31,6 @@
         <h3><?php echo __('Output Formats'); ?></h3>
         <div class="element-text"><?php echo output_format_list(); ?></div>
     </div>
-    
-    <nav>
-        <ul class="item-pagination navigation">
-            <li id="previous-item" class="previous"><?php echo link_to_previous_item_show(); ?></li>
-            <li id="next-item" class="next"><?php echo link_to_next_item_show(); ?></li>
-        </ul>
-    </nav>
     
 </aside>
 
