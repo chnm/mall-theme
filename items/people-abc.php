8ca14530c5a33d151b317cdd038962575f021822
Kim Nguyen <design@kimisgold.com>
2013-08-19 16:47:25 -0400

    Alphabetize people item types by last name in Omeka for the list view.

--- items/people-abc.php
+++ items/people-abc.php
@@ -1,74 +1,31 @@
-<?php include('header.php'); ?>
-<body class="people alpha">
+<div role="main">
 
-    <header>
-        <h1 id="site-title"><div class="logo"></div><a href="index">Histories of the<br />National Mall</a></h1>
-        
-        <?php include('search.php'); ?>
-        
-        <?php include('navigation.php'); ?>
-    </header>
-
-    <div role="main">
+    <h1>Browse People</h1>
     
-        <h1>Browse People</h1>
-        
-        <nav>
-            <ul>
-                <li class="by-image"><a href="people-image"><span class="screen-reader-text">By image</span></a></li>
-                <li class="alphabetically"><a href="people-abc"><span class="screen-reader-text">Alphabetically</span></a></li>
-            </ul>
-        </nav>
-        
-        <div class="persons">
-        
-            <div class="letter a">
-                
-                <h2>A</h2>
-                
-                <span class="person"><a href="#">Archie Alphonso Alexander</a></span>
-                
+    <nav>
+        <ul>
+            <li class="by-image"><a href="people-image"><span class="screen-reader-text">By image</span></a></li>
+            <li class="alphabetically"><a href="people-abc"><span class="screen-reader-text">Alphabetically</span></a></li>
+        </ul>
+    </nav>
+    
+    <div class="persons">
+    
+    <?php $people = get_records('Item', array('item_type' => 'people', 'sort_field' => 'Item Type Metadata,Last Name'), 9999); ?>
+    <?php $letter = ''; ?>
+    <?php foreach ($people as $person): ?>
+        <?php $firstOfLast = substr(metadata($person, array('Item Type Metadata', 'Last Name')),0,1); ?>
+        <?php if (strcmp($letter, $firstOfLast) !== 0): ?>
+            <?php if (strcmp($letter, '') !== 0): ?>
             </div>
-
-            <div class="letter h">
-                
-                <h2>H</h2>
-                
-                <span class="person"><a href="#">William Temple Hornaday</a></span>
-                
-            </div>
-
-            <div class="letter j">
-                
-                <h2>J</h2>
-                
-                <span class="person"><a href="#">Cleve Jones</a></span>
-                
-            </div>
+            <?php endif; ?>
+            <?php $letter = $firstOfLast; ?>
+            <div class="letter <?php echo $letter; ?>">
+                <h2><?php echo $letter; ?></h2>
+        <?php endif; ?>
+                    <span class="person"><?php echo link_to_item(metadata($person, array('Dublin Core', 'Title')), array(), 'show', $person); ?></span>
+    <?php endforeach; ?>
                         
-            <div class="letter s">
-                
-                <h2>S</h2>
-                
-                <span class="person"><a href="#">Richard H. Sylvester</a></span>
-                
-            </div>
-                        
-            <div class="letter s">
-                
-                <h2>W</h2>
-                
-                <span class="person"><a href="#">George Washington</a></span>
-                
-                <span class="person"><a href="#">Walter W. Waters</a></span>
-                
-                <span class="person"><a href="#">W. Richard West, Jr.</a></span>
-                
-                <span class="person"><a href="#">Roy Wilkins</a></span>
-                
-            </div>
-                            
-        </div>
-        
     </div>
-<?php include('footer.php'); ?>
+    
+</div>
