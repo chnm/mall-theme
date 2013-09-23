bfeabbe4af9d7473545c57a771d10d46411557d7
Kim Nguyen <design@kimisgold.com>
2013-09-23 14:55:41 -0400

    Add simple pages view.

---
+++ simple-pages/page/show.php
@@ -0,0 +1,18 @@
+<?php echo head(array(
+    'title' => metadata('simple_pages_page', 'title'),
+    'bodyclass' => 'page simple-page',
+    'bodyid' => metadata('simple_pages_page', 'slug')
+)); ?>
+<div role="main">
+    <h1><?php echo metadata('simple_pages_page', 'title'); ?></h1>
+    <?php
+    $text = metadata('simple_pages_page', 'text', array('no_escape' => true));
+    if (metadata('simple_pages_page', 'use_tiny_mce')) {
+        echo $text;
+    } else {
+        echo eval('?>' . $text);
+    }
+    ?>
+</div>
+
+<?php echo foot(); ?>
