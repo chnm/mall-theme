2063394742fbf1eb9694e4d70bc93a75a8e87dfe
Kim Nguyen <design@kimisgold.com>
2013-10-08 22:26:55 -0400

    Add thumbnails to search. Fix #11.

--- search/index.php
+++ search/index.php
@@ -10,7 +10,25 @@ $query = $_GET['query'];
     <?php echo pagination_links(); ?>
             <?php foreach (loop('search_texts') as $searchText): ?>
             <?php $record = get_record_by_id($searchText['record_type'], $searchText['record_id']); ?>
-                <div class="search-result <?php echo strtolower($searchText['record_type']); ?>"><a href="<?php echo record_url($record, 'show'); ?>"><?php echo $searchText['title'] ? $searchText['title'] : '[Unknown]'; ?></a></div>
+            <?php $searchRecordType = $searchText['record_type']; ?>
+                <div class="search-result <?php echo strtolower($searchRecordType); ?>">
+                    <?php if ($searchRecordType == 'Item' && metadata($record, 'item_type_name')): ?>
+                        <?php if (metadata($record, 'has_thumbnail')): ?>
+                            <?php echo link_to_item(item_image('square_thumbnail', array(), 0, $record), array('class' => 'image'), 'show', $record); ?>
+                        <?php endif; ?>
+                    <?php elseif ($searchRecordType == 'Exhibit'): ?>
+                        <?php
+                            $exhibitId = $record->id;
+                            $exhibitItem = get_records('Item', array('exhibit' => $exhibitId, 'random' => true, 'has files' => true), 1);
+                            $exhibitImage = get_db()->getTable('File')->findWithImages($exhibitItem[0]->id, 0);
+                            echo link_to($record, 'show', file_image('square_thumbnail', array(), $exhibitImage), array('class' => 'image'));
+                        ?>
+                    <?php endif; ?>
+                        <h3><a href="<?php echo record_url($record, 'show'); ?>"><?php echo $searchText['title'] ? $searchText['title'] : '[Unknown]'; ?></a></h3>
+                    <?php if ($searchRecordType == 'Item' && $desc = metadata($record, array('Dublin Core', 'Description'), array('snippet' => 250))): ?>
+                        <div class="description"><?php echo $desc; ?></div>
+                    <?php endif; ?>
+                </div>
             <?php endforeach; ?>
     <?php echo pagination_links(); ?>
     <?php else: ?>
